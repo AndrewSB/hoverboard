e7eaad6c4a170a6f797d3b05d752e520234c91a1
Zachary Adam Kaplan <razic@viralkitty.com>
2015-03-01 19:30:17 -0800

    add central

---
+++ central/Hoverboard/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Hoverboard
+//
+//  Created by Zachary Adam Kaplan on 3/1/15.
+//  Copyright (c) 2015 viralkitty. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
